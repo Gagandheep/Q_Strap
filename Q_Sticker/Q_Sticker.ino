ff825de518116caa5fdddafcfc03d392925ef0f6
Gagandheep <gagandheepmr@gmail.com>
2020-04-10 16:54:50 +0530

    added comments

--- Q_Sticker/Q_Sticker.ino
+++ Q_Sticker/Q_Sticker.ino
@@ -1,14 +1,34 @@
+ /*
+ * Team Id: 238
+ * Author List:  Adarsh, Bhargav, Gagandheep, Yasho Mangal
+ * Filename: Q_Sticker
+ * Functions: send_message, setup , loop
+ * Global Variables: Q_num, contacts, time_secs, reported, ssid, password
+ */
+
+// Library imports
 #include <ESP8266WiFi.h>
 
-#define NOS 2
-#define NORMAL_CHECK_DELAY 12000
-#define CRITICAL_CHECK_DELAY 5000
-#define MAX_OFFLINE_DURATION 10000
+#define NOS 2                         // constant to hold the number of contacts to which alert has to be sent
+#define NORMAL_CHECK_DELAY 12e7       // time (us) delay between connection checks when all straps are connected
+#define CRITICAL_CHECK_DELAY 5000     // time (ms) delay between connection checks when all straps are not connected
+#define MAX_OFFLINE_DURATION 10000    // time (ms) duration during which the sticker waits for the straps to reconnect if Disconnected, after which an alert is sent
+#define PIN_BUZZER 5                  // pin to which buzzer is connected
 
-int Q_num = 1;
-String contacts[NOS] = { "\"+910000000000\"", "\"+910123456789\""};
-unsigned long time_secs = 0;
-bool reported = false;
+int Q_num = 1;                                                        // number of straps to be connected
+String contacts[NOS] = { "\"+910000000000\"", "\"+910123456789\""};   // list of contacts
+unsigned long time_secs = 0;                                          // variable to measure strap offline durartion
+bool reported = false;                                                // true if message is sent
+const char* ssid = "Sticker_01";                                      // SSID
+const char* password = "1234pass";                                    // password
+
+ /*
+ * Function Name: send_message
+ * Input: None
+ * Output: None
+ * Description: Sends text message to the phone numbers present in the list 'contacts' 
+ * Example Call: send_message();
+ */
 
 void send_message() {
   for (int i = 0; i < NOS; i++) {
@@ -16,58 +36,65 @@ void send_message() {
     Serial.println("AT+CMGF=1");  // Set GSM in text mode
     delay(1000);                  // One second delay
     Serial.println();
-    Serial.print("AT+CMGS=");     // Enter the receiver number
+    Serial.print("AT+CMGS=");     // Enters the receiver number
     Serial.print(contacts[i]);
     Serial.println();
     delay(100);
-    Serial.print("Disconnected"); // SMS body - Sms Text
+    Serial.print("Disconnected"); // SMS body
     delay(1000);
     Serial.println();
-    Serial.write(26);
+    Serial.write(26);             // Send Text
   }
 }
 
-void setup() {
-  //Begin nodemcu serial-0 channel
-  Serial.begin(9600);
-  pinMode(5, OUTPUT);
+ /*
+ * Function Name: setup
+ * Input: None
+ * Output: None
+ * Description: arduino setup function
+ */
 
-  Serial.print("Setting soft-AP ... ");
-  boolean result = WiFi.softAP("Sticker_01", "1234pass");
-  if (result == true)
+void setup() {
+
+  Serial.begin(9600);                             // Begin nodemcu serial-0 channel
+  pinMode(PIN_BUZZER, OUTPUT);                    // Set buzzer pin as output
+
+  boolean result = WiFi.softAP(ssid, password);   // Setup nodemcu in softAP mode
+  if (result == true)                             // if setup was successful
   {
-    Serial.println("Ready");
-    Serial.print("AT");  //Start Configuring GSM Module
+    Serial.print("AT");                           //Start Configuring GSM Module
     delay(10000);
 
   }
   else
   {
-    Serial.println("Failed!");
+    digitalWrite (PIN_BUZZER, HIGH);              // Set buzzer pin to HIGH
   }
 }
 
+ /*
+ * Function Name: loop
+ * Input: None
+ * Output: None
+ * Description: arduino loop function
+ */
+
 void loop() {
   
-  Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
-  if(WiFi.softAPgetStationNum() == Q_num){
-    Serial.println("ESP going to deep sleep for required checking time");
-    Serial.println();
-    delay(10000);
-    ESP.deepSleep(2e7);
+  if(WiFi.softAPgetStationNum() == Q_num){        // Check whether all straps are conneted
+    ESP.deepSleep(NORMAL_CHECK_DELAY);            // deepSleep if all straps are connected
   }
-  //delay(NORMAL_CHECK_DELAY);
-  while (WiFi.softAPgetStationNum() != Q_num) {
-    Serial.println("checking connection again");
-    delay(CRITICAL_CHECK_DELAY);
+
+  while (WiFi.softAPgetStationNum() != Q_num) {   // while any of the straps is disconnected
+    delay(CRITICAL_CHECK_DELAY); 
     time_secs += CRITICAL_CHECK_DELAY;
     if (time_secs >= MAX_OFFLINE_DURATION && !reported) {
-      send_message();
+      send_message();                             // Report
       reported = true;
-      while (WiFi.softAPgetStationNum() != Q_num) {
-        digitalWrite (5, HIGH);
+      while (WiFi.softAPgetStationNum() != Q_num) {   // Beep Buzzer continuously until all straps reconnect
+        digitalWrite (PIN_BUZZER, HIGH);
         delay(500);
-        digitalWrite(5, LOW);
+        digitalWrite(PIN_BUZZER, LOW);
         delay(500);
       }
     }
--- Q_Strap/Q_Strap.ino
+++ Q_Strap/Q_Strap.ino
@@ -1,26 +1,49 @@
+ /*
+ * Team Id: 238
+ * Author List:  Adarsh, Bhargav, Gagandheep, Yasho Mangal
+ * Filename: Q_Strap
+ * Functions: setup , loop
+ * Global Variables: ssid, password
+ */
+
+// Library imports
  #include <ESP8266WiFi.h>
- 
-const char* ssid = "Sticker_01";
-const char* password = "1234pass";
+
+ #define PIN_BUZZER 5                   // pin to which buzzer is connected
+  
+const char* ssid = "Sticker_01";        // SSID
+const char* password = "1234pass";      // password
+
+
+ /*
+ * Function Name: setup
+ * Input: None
+ * Output: None
+ * Description: arduino setup function
+ */
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(5,OUTPUT);
-  WiFi.begin(ssid, password); 
+  pinMode(PIN_BUZZER, OUTPUT);        // set pin 5 as Output
+  WiFi.begin(ssid, password);         // connect to Sticker
+
+  delay(2000);
 }
 
+/*
+ * Function Name: loop
+ * Input: None
+ * Output: None
+ * Description: arduino loop function
+ */
+
 void loop() {
-  delay(2000);
   if(WiFi.status() == WL_CONNECTED){
-    Serial.println("connected");
-    Serial.println("going deep sleep for 5 sec");
-    ESP.deepSleep(5e6);
+    ESP.deepSleep(5e6);               // deepSleep for 5 seconds
   }
-  else{
-    digitalWrite (5,HIGH);
+  else{                               // Beep buzzer continuously
+    digitalWrite (PIN_BUZZER, HIGH);  // set buzzer pin to HIGH
     delay(500);
-    digitalWrite(5,LOW); 
-    Serial.println("not connected");
+    digitalWrite(PIN_BUZZER, LOW);    // set buzzer pin to LOW
     delay(500);
   }
 }

c19ba92018ad9bdea03052a814953b921eaf8024
Gagandheep <gagandheepmr@gmail.com>
2020-04-10 17:00:29 +0530

    code formatting

--- Q_Sticker/Q_Sticker.ino
+++ Q_Sticker/Q_Sticker.ino
@@ -6,6 +6,7 @@
  * Global Variables: Q_num, contacts, time_secs, reported, ssid, password
  */
 
+
 // Library imports
 #include <ESP8266WiFi.h>
 
@@ -15,12 +16,14 @@
 #define MAX_OFFLINE_DURATION 10000    // time (ms) duration during which the sticker waits for the straps to reconnect if Disconnected, after which an alert is sent
 #define PIN_BUZZER 5                  // pin to which buzzer is connected
 
+
 int Q_num = 1;                                                        // number of straps to be connected
 String contacts[NOS] = { "\"+910000000000\"", "\"+910123456789\""};   // list of contacts
 unsigned long time_secs = 0;                                          // variable to measure strap offline durartion
 bool reported = false;                                                // true if message is sent
 const char* ssid = "Sticker_01";                                      // SSID
 const char* password = "1234pass";                                    // password
+
 
  /*
  * Function Name: send_message
@@ -47,6 +50,7 @@ void send_message() {
   }
 }
 
+
  /*
  * Function Name: setup
  * Input: None
@@ -60,17 +64,15 @@ void setup() {
   pinMode(PIN_BUZZER, OUTPUT);                    // Set buzzer pin as output
 
   boolean result = WiFi.softAP(ssid, password);   // Setup nodemcu in softAP mode
-  if (result == true)                             // if setup was successful
-  {
-    Serial.print("AT");                           //Start Configuring GSM Module
+  if (result == true) {                           // if setup was successful 
+    Serial.print("AT");                           // Start Configuring GSM Module
     delay(10000);
-
   }
-  else
-  {
+  else {
     digitalWrite (PIN_BUZZER, HIGH);              // Set buzzer pin to HIGH
   }
 }
+
 
  /*
  * Function Name: loop
@@ -81,16 +83,18 @@ void setup() {
 
 void loop() {
   
-  if(WiFi.softAPgetStationNum() == Q_num){        // Check whether all straps are conneted
+  if(WiFi.softAPgetStationNum() == Q_num) {       // Check whether all straps are conneted
     ESP.deepSleep(NORMAL_CHECK_DELAY);            // deepSleep if all straps are connected
   }
 
   while (WiFi.softAPgetStationNum() != Q_num) {   // while any of the straps is disconnected
     delay(CRITICAL_CHECK_DELAY); 
     time_secs += CRITICAL_CHECK_DELAY;
+
     if (time_secs >= MAX_OFFLINE_DURATION && !reported) {
       send_message();                             // Report
       reported = true;
+
       while (WiFi.softAPgetStationNum() != Q_num) {   // Beep Buzzer continuously until all straps reconnect
         digitalWrite (PIN_BUZZER, HIGH);
         delay(500);
@@ -99,6 +103,7 @@ void loop() {
       }
     }
   }
+
   time_secs = 0;
   reported = false;
 }
